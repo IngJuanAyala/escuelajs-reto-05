6859097c3ef44a17543bc8d0d032ceb1a55f5c53
JuanAyal <juanayalasalazar1@gmail.com>
2019-09-28 11:46:08 -0500

    solución reto 05 escuela platzi

--- src/index.js
+++ src/index.js
@@ -1,29 +1,62 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
+// const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
+
 
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
+      debugger;
+      //Primer problema:
+      if(response.info.next  !== "" || response.info.next !== null)
+      localStorage.setItem('next_fetch', response.info.next);
+
+
       const characters = response.results;
-      let output = characters.map(character => {
-        return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
+      //const characters = undefined;
+        
+      //Cuarto problema
+      if (characters === undefined || characters.length == 0) {
+        
+        var div = document.getElementById('Messaje');
+        div.innerHTML = 'Ya no hay personajes...';
+
+        observer.unobserve(document.getElementById("$observe"));
+        
+      }else{
+
+        let output = characters.map(character => {
+          return `
+        <article class="Card">
+          <img src="${character.image}" />
+          <h2>${character.name}<span>${character.species}</span></h2>
+        </article>
+      `
+        }).join('');
+        let newItem = document.createElement('section');
+        newItem.classList.add('Items');
+        newItem.innerHTML = output;
+        $app.appendChild(newItem);
+
+      }
+
+      
+
     })
     .catch(error => console.log(error));
 }
 
 const loadData = () => {
+
+ //Segundo problema:
+debugger;
+ var next_fetch = localStorage.getItem('next_fetch'); 
+ if (next_fetch !== null){
+  getData(next_fetch);
+ }
+
   getData(API);
 }
 
@@ -34,5 +67,12 @@ const intersectionObserver = new IntersectionObserver(entries => {
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
+
+
+//Tercer problema
+window.onbeforeunload = function() {
+  localStorage.removeItem('next_fetch');
+  return '';
+};
 
 intersectionObserver.observe($observe);
